53e6297146d75bd96229a1fe07f4f4a64126b17d
denise.leon <denise.leon@elev.ga.ntig.se>
2024-02-21 10:47:46 +0100

    First

---
+++ src/Files/MyFile.java
@@ -0,0 +1,60 @@
+package Files;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class MyFile {
+    String fileName = "myFile.txt";
+    File myFileObject;
+
+    public void MyFiles() {
+        createFile(); //creates a file
+        //write2File(); //Writes to a file
+        readFromFile();
+    }
+
+    private void readFromFile() {
+        try {
+            Scanner myFileScanner = new Scanner(myFileObject);
+            while (myFileScanner.hasNext()) {
+                System.out.println(myFileScanner.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read from file");
+        }
+
+    }
+
+    private void write2File() {
+        try {
+            FileWriter myWriterObject = new FileWriter(fileName, true);
+            Scanner myKeyScanner = new Scanner(System.in);
+            System.out.println("What do you want to write in the file?");
+            myWriterObject.write(myKeyScanner.nextLine());
+            myWriterObject.close();
+            System.out.println("I wrote to file " + fileName);
+        } catch (IOException e) {
+            System.out.println("could not write to file");
+        }
+    }
+
+    public void createFile() {
+        myFileObject = new File(fileName);
+        try {
+            if (myFileObject.createNewFile()) {
+                System.out.println(fileName + " has been created");
+            } else {
+                System.out.println(fileName + " already exists");
+            }
+
+        } catch (IOException ioErr) {
+            System.out.println("file could not be created");
+
+        } catch (Exception generalException) {
+            System.out.println("something went wrong");
+        }
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.nio.file.Files;
+
+import Files.MyFile;
+
 public class Main {
+
     public static void main(String[] args) {
+        new MyFile();
         System.out.println("Hello world!");
     }
 }
